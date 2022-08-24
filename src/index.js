69bcc1a3012bd2534d5292ad7c1c6dc7b8193004
Brenda <waiyegobrenda99@gmail.com>
2022-08-24 13:19:53 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+const form = document.querySelector('form')
+form.addEventListener('submit', onSubmit);
+function onSubmit(e) {
+  e.preventDefault();
+  pasteTask(e.target["new-task-description"].value)
+}
+function pasteTask(task) {
+  let p = document.createElement('p')
+  p.textContent = task
+  console.log(p)
+ document.querySelector("#list").appendChild(p)
+
+}
+  console.log ("After DOM loaded")
 });
+console.log('Before DOM loads')
